14995f54bfbfa7edb8d2247c15d293200eeaa5f6
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-17 22:59:51 +0530

    Added Finding Maximum Float Value Program

---
+++ Generics_13/FindMaxFloatUC_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_13
+{
+    public  class FindMaxFloatUC_2
+    {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
--- Generics_13/Program.cs
+++ Generics_13/Program.cs
@@ -7,9 +7,15 @@ namespace Generics_13
         public static void Main(string[] args)
         {
             Console.WriteLine("Finding Maximum Numbers By Using Generics");
+            //UC_1
             int[] intArray = { 100, 75, 50 };
             GenericMaximum<int> generics = new GenericMaximum<int>(intArray);
             generics.PrintMaxValue();
+            Console.WriteLine("-----------------------------------");
+            //UC_2
+            float[] floatArray = { 1.50f, 0.75f, 5.0f };
+            GenericMaximum<float> genericFloat = new GenericMaximum<float>(floatArray);
+            genericFloat.PrintMaxValue();
             Console.WriteLine("-----------------------------------");
 
         }
